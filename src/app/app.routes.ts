7f8371c3f6196a48716eff8ecd9a566cff1b0940
Balmukhambet Darkhan <darhan.balmukhambet@gmail.com>
2025-11-25 22:38:00 +0500

    fix: route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { login_page } from '@auth/constants';
 import { authGuard } from '@auth/guards';
+import { LoginComponent } from '@auth/pages';
 
 export const routes: Routes = [
+  {
+    path: login_page,
+    component: LoginComponent,
+  },
   {
     path: '',
     canActivate: [authGuard],
     loadChildren: () => import('./features/orders.route').then((r) => r.orders_route),
   },
-  {
-    path: login_page,
-    loadComponent: () => import('./auth/pages').then((c) => c.LoginComponent),
-  },
+  { path: '**', redirectTo: '' },
 ];
